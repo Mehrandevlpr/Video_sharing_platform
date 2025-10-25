be4276bbc40f73b9d5dfaf8d990060d03f5963bd
Mehrandevlpr <mehranhamzeh74@gmail.com>
2025-10-25 06:13:01 -0500

    Implementing Jobs and Queue

--- app/Mail/verfiyEmail.php
+++ app/Mail/verfiyEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class verfiyEmail extends Mailable
+class verfiyEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
     public $user;
